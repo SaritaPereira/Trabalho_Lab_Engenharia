ec16be13c132e981dd15487b3cafb621092529d6
SaritaPereira <saritapg2010@hotmail.com>
2025-03-22 21:12:47 -0300

    Atualizado codigo Semana5

--- Semana5/script.js
+++ Semana5/script.js
@@ -1,86 +1,109 @@
-const divContador = document.getElementById("contador");
+document.addEventListener("DOMContentLoaded", function () {
+    const divContador = document.getElementById("contador");
 
-const container = document.createElement("div");
-container.classList.add("container");
+    const container = document.createElement("div");
+    container.classList.add("container");
 
-const titulo = document.createElement("h2");
-titulo.innerText = "Contador de Pessoas";
-s
-const totalTexto = document.createElement("p");
-totalTexto.innerHTML = "<strong>Total:</strong> ";
-const totalNumero = document.createElement("span");
-totalNumero.classList.add("total");
-totalNumero.innerText = "0";
-totalTexto.appendChild(totalNumero);
+    // Seção do Total
+    const totalBox = document.createElement("div");
+    totalBox.classList.add("total-box");
 
-const homens = criarContador("Homens", "Semana5/img/homem.png");
-const mulheres = criarContador("Mulheres", "Semana5/img/mulher.png");
+    const titulo = document.createElement("h2");
+    titulo.innerText = "Total";
 
-const btnReset = document.createElement("button");
-const imgReset = document.createElement("img");
-imgReset.src = "Semana5/img/reset.png";
-imgReset.alt = "Resetar";
-btnReset.appendChild(imgReset);
-btnReset.onclick = () => {
-    homens.numero.innerText = "0";
-    mulheres.numero.innerText = "0";
-    atualizarTotal();
-};
+    const totalNumero = document.createElement("input");
+    totalNumero.setAttribute("type", "text");
+    totalNumero.setAttribute("readonly", "true");
+    totalNumero.classList.add("total-numero");
+    totalNumero.value = "0";
 
-container.appendChild(titulo);
-container.appendChild(totalTexto);
-container.appendChild(homens.box);
-container.appendChild(mulheres.box);
-container.appendChild(btnReset);
-divContador.appendChild(container);
-
-function criarContador(tipo, imagemSrc) {
-    const box = document.createElement("div");
-    box.classList.add("contador-box");
-
-    const imagem = document.createElement("img");
-    imagem.src = imagemSrc;
-    imagem.alt = tipo;
-
-    const label = document.createElement("p");
-    label.innerText = tipo;
-
-    const btnMenos = document.createElement("button");
-    const imgMenos = document.createElement("img");
-    imgMenos.src = "Semana4/img/subtrair.png";
-    imgMenos.alt = "Subtrair";
-    btnMenos.appendChild(imgMenos);
-    btnMenos.onclick = () => {
-        let valor = parseInt(numero.innerText);
-        if (valor > 0) {
-            numero.innerText = valor - 1;
-            atualizarTotal();
-        }
-    };
-
-    const numero = document.createElement("span");
-    numero.innerText = "0";
-
-    const btnMais = document.createElement("button");
-    const imgMais = document.createElement("img");
-    imgMais.src = "Semana4/img/adicionar.png";
-    imgMais.alt = "Adicionar";
-    btnMais.appendChild(imgMais);
-    btnMais.onclick = () => {
-        numero.innerText = parseInt(numero.innerText) + 1;
+    const btnReset = document.createElement("button");
+    btnReset.classList.add("btn-reset");
+    btnReset.innerText = "Resetar";
+    btnReset.onclick = () => {
+        homens.numero.innerText = "0";
+        mulheres.numero.innerText = "0";
         atualizarTotal();
     };
 
-    box.appendChild(imagem);
-    box.appendChild(label);
-    box.appendChild(btnMenos);
-    box.appendChild(numero);
-    box.appendChild(btnMais);
+    totalBox.appendChild(titulo);
+    totalBox.appendChild(totalNumero);
+    totalBox.appendChild(btnReset);
 
-    return { box, numero };
-}
+    // Criando contadores lado a lado
+    const contadoresBox = document.createElement("div");
+    contadoresBox.classList.add("contadores-box");
 
-function atualizarTotal() {
-    totalNumero.innerText =
-        parseInt(homens.numero.innerText) + parseInt(mulheres.numero.innerText);
-}
+    const homens = criarContador("Homens", "img/homem.png");
+    const mulheres = criarContador("Mulheres", "img/mulher.png");
+
+    contadoresBox.appendChild(homens.box);
+    contadoresBox.appendChild(mulheres.box);
+
+    container.appendChild(totalBox);
+    container.appendChild(contadoresBox);
+    divContador.appendChild(container);
+
+    function criarContador(tipo, imagemSrc) {
+        const box = document.createElement("div");
+        box.classList.add("contador-box");
+
+        const imagem = document.createElement("img");
+        imagem.src = imagemSrc;
+        imagem.alt = tipo;
+        imagem.classList.add("contador-img");
+
+        const label = document.createElement("p");
+        label.innerText = tipo;
+
+        const btnMais = document.createElement("button");
+        btnMais.classList.add("btn-controle", "btn-mais");
+        const imgMais = document.createElement("img");
+        imgMais.src = "img/adicionar.png";
+        imgMais.alt = "Adicionar";
+        btnMais.appendChild(imgMais);
+        btnMais.onclick = () => {
+            numero.innerText = parseInt(numero.innerText) + 1;
+            atualizarTotal();
+        };
+
+        const btnMenos = document.createElement("button");
+        btnMenos.classList.add("btn-controle", "btn-menos");
+        const imgMenos = document.createElement("img");
+        imgMenos.src = "img/subtrair.png";
+        imgMenos.alt = "Subtrair";
+        btnMenos.appendChild(imgMenos);
+        btnMenos.onclick = () => {
+            let valor = parseInt(numero.innerText);
+            if (valor > 0) {
+                numero.innerText = valor - 1;
+                atualizarTotal();
+            }
+        };
+
+        const numero = document.createElement("span");
+        numero.classList.add("contador-numero");
+        numero.innerText = "0";
+
+        const numeroBox = document.createElement("div");
+        numeroBox.classList.add("numero-box");
+        numeroBox.appendChild(numero);
+
+        const botoes = document.createElement("div");
+        botoes.classList.add("botoes-box");
+        botoes.appendChild(btnMenos);
+        botoes.appendChild(btnMais);
+
+        box.appendChild(imagem);
+        box.appendChild(label);
+        box.appendChild(botoes);
+        box.appendChild(numeroBox);
+
+        return { box, numero };
+    }
+
+    function atualizarTotal() {
+        totalNumero.value =
+            parseInt(homens.numero.innerText) + parseInt(mulheres.numero.innerText);
+    }
+});
